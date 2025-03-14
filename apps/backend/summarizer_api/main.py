7231cfc934e2bd2946f3de7bf719edb1bb340615
Farasat Ali <farasataliazeemi1@gmail.com>
2025-03-14 19:31:11 +0500

    upd: package

--- apps/backend/summarizer_api/main.py
+++ apps/backend/summarizer_api/main.py
@@ -2,10 +2,12 @@ import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-from logging_config import configure_logging
+from summarizer_api.logging_config import configure_logging
 
 logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     configure_logging()
+
+app = FastAPI()
